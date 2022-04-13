0c708b11814cbc1cb5bc488a3e639edf39d27661
Remigiusz <repogplus@gmail.com>
2022-04-13 23:15:00 +0200

    Stage completed.

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,65 +1,56 @@
 package correcter;
 
-import java.time.temporal.ValueRange;
-import java.util.Arrays;
+import java.io.InputStream;
+import java.io.FileInputStream;
 import java.util.Random;
-import java.util.Scanner;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        char[] input = scanner.nextLine().toCharArray();
-        final ValueRange[] ACCEPTABLE_SYMBOL_RANGES = {
-                ValueRange.of('a', 'z'),
-                ValueRange.of('A', 'Z'),
-                ValueRange.of('0', '9'),
-                ValueRange.of(' ', ' ')
-        };
-        String symbolRange = Arrays.toString(ACCEPTABLE_SYMBOL_RANGES);
-        System.out.println(String.valueOf(input));
-        char[] tripledInput = tripleInput(input);
-        char[] errors = createErrors(tripledInput, symbolRange);
-        System.out.println(deleteErrors(errors));
+    public static void main(String... args) {
+
+        Correcter msg = new Correcter();
+        String fileNameIn = "send.txt";
+        String fileNameOut = "received.txt";
+
+        msg.setBytes(fileNameIn);
+        msg.corruptBytes();
+        msg.writeBytes(fileNameOut);
 
     }
+}
 
-    public static char[] tripleInput(char[] input) {
-        char[] tripledInput = new char[input.length * 3];
-        for (int i = 0; i < input.length; i++) {
-            int repeatChar = 0;
-            while (repeatChar < 3) {
-                tripledInput[i * 3 + repeatChar] = input[i];
-                repeatChar++;
-            }
+class Correcter {
+
+    private static byte[] bytes;
+
+    void setBytes(String fileNameIn) {
+        try (InputStream inputStream = new FileInputStream(fileNameIn)) {
+            bytes = inputStream.readAllBytes();
+        } catch (FileNotFoundException fnf) {
+            System.out.println("File not found!");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getCause());
         }
-        System.out.println(String.valueOf(tripledInput));
-        return tripledInput;
     }
 
-    public static char[] createErrors(char[] tripledInput, String symbolRange) {
-        Random random = new Random();
-        int errorIndex;
-        for (int i = 0; i < tripledInput.length; i += 3) {
-            errorIndex = i + random.nextInt(3);
-            if (errorIndex < tripledInput.length) {
-                char newChar = symbolRange.charAt(random.nextInt(symbolRange.length()));
-                tripledInput[errorIndex] = tripledInput[errorIndex] == newChar ? (char) (newChar - 1) : newChar;
-            }
+    void corruptBytes() {
+        Random generate = new Random();
+
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] ^= 1 << generate.nextInt(8);
         }
-        System.out.println(String.valueOf(tripledInput));
-        return tripledInput;
     }
 
-    public static String deleteErrors(char[] errors) {
-        char[] corrected = new char[errors.length / 3];
-        for (int i = 0, j = 0; i < errors.length; i += 3) {
-            if (errors[i] == errors[i + 1] || errors[i] == errors[i + 2]) {
-                corrected[j] = errors[i];
-            } else {
-                corrected[j] = errors[i + 1];
-            }
-            j++;
+    void writeBytes(String out) {
+        try (OutputStream outputStream = new FileOutputStream(out, false)) {
+            outputStream.write(bytes);
+        } catch (FileNotFoundException fnf) {
+            System.out.println("File not found!");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getCause());
         }
-        return String.valueOf(corrected);
     }
 }
