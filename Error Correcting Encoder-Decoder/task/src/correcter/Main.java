9a7c4fb239a94c2c3d74d6e2d35ede5a8458bde2
Remigiusz <repogplus@gmail.com>
2022-04-14 23:52:35 +0200

    Stage completed.

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,56 +1,147 @@
 package correcter;
 
-import java.io.InputStream;
-import java.io.FileInputStream;
+import java.io.*;
 import java.util.Random;
-import java.io.OutputStream;
-import java.io.FileOutputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String... args) {
-
-        Correcter msg = new Correcter();
-        String fileNameIn = "send.txt";
-        String fileNameOut = "received.txt";
-
-        msg.setBytes(fileNameIn);
-        msg.corruptBytes();
-        msg.writeBytes(fileNameOut);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a mode: ");
+        String mode = scanner.nextLine();
+        switch (mode) {
+            case "encode":
+                encode();
+                break;
+            case "send":
+                send();
+                break;
+            case "decode":
+                decode();
+                break;
+            default:
+                System.out.println("Write valid mode.");
+        }
 
     }
-}
 
-class Correcter {
-
-    private static byte[] bytes;
-
-    void setBytes(String fileNameIn) {
-        try (InputStream inputStream = new FileInputStream(fileNameIn)) {
-            bytes = inputStream.readAllBytes();
-        } catch (FileNotFoundException fnf) {
-            System.out.println("File not found!");
-        } catch (IOException ioe) {
-            System.out.println(ioe.getCause());
+    public static void encode() {
+        // 1 sposób zczytania danych z pliku przez inputstream
+        File file = new File("send.txt");
+        int numBytes = (int) file.length();
+        int numBits = numBytes * 8;
+        int numOutBytes = numBits / 3 + (numBits % 3 == 0 ? 0 : 1);
+        byte[] data = new byte[numBytes];
+        byte[] dataOut = new byte[numOutBytes];
+        try (FileInputStream fis = new FileInputStream(file)) {
+            fis.read(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < numOutBytes; i++) {
+            dataOut[i] = 0;
+            int xored = 0;
+            for (int j = 0; j < 3; j++) {
+                int bitIndex = i * 3 + j;
+                int byteIndex = bitIndex / 8;
+                int bytePos = bitIndex % 8;
+                int bit;
+                if (byteIndex >= data.length) {
+                    bit = 0;
+                } else {
+                    bit = getBit(data[byteIndex], bytePos);
+                }
+                xored ^= bit;
+                dataOut[i] = setBit(dataOut[i], bit, j * 2);
+                dataOut[i] = setBit(dataOut[i], bit, j * 2 + 1);
+            }
+            dataOut[i] = setBit(dataOut[i], xored, 6);
+            dataOut[i] = setBit(dataOut[i], xored, 7);
+        }
+        File outFile = new File("encoded.txt");
+        try (FileOutputStream fos = new FileOutputStream(outFile)) {
+            fos.write(dataOut);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    void corruptBytes() {
+    public static int getBit(byte b, int index) {
+        return (b & (1 << (7 - index))) == 0 ? 0 : 1;
+    }
+
+    public static byte setBit(byte b, int bit, int index) {
+        return (byte) (b | (bit << (7 - index)));
+    }
+
+
+    public static void corruptBytes(byte[] inp) {
         Random generate = new Random();
-
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] ^= 1 << generate.nextInt(8);
+        for (int i = 0; i < inp.length; i++) {
+            inp[i] ^= 1 << generate.nextInt(8);
         }
     }
 
-    void writeBytes(String out) {
-        try (OutputStream outputStream = new FileOutputStream(out, false)) {
-            outputStream.write(bytes);
-        } catch (FileNotFoundException fnf) {
-            System.out.println("File not found!");
-        } catch (IOException ioe) {
-            System.out.println(ioe.getCause());
+    public static void send() {
+        // 2 sposób zczytania danych z pliku przez inputstream
+        File file = new File("encoded.txt");
+        byte[] data = null;
+        try (InputStream inputStream = new FileInputStream(file)) {
+            data = inputStream.readAllBytes();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        corruptBytes(data);
+        File outFile = new File("received.txt");
+        try (OutputStream outputStream = new FileOutputStream(outFile)) {
+            outputStream.write(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static byte decodeByte(byte input) {
+        int xorall = 0;
+        for (int i = 0; i < 4; i++) {
+            xorall ^= getBit(input, i * 2);
+        }
+        for (int i = 0; i < 4; i++) {
+            if (getBit(input, i * 2) != getBit(input, i * 2 + 1)) {
+                return (byte) (input ^ (xorall << (7 - i * 2)));
+            }
+        }
+        return input;
+    }
+
+    public static void decode() {
+        File file = new File("received.txt");
+        byte[] data = null;
+        try (InputStream inputStream = new FileInputStream(file)) {
+            data = inputStream.readAllBytes();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int outputBytes = (data.length * 3) / 8;
+        byte[] outData = new byte[outputBytes];
+        int index = 0;
+        for (int i = 0; i < data.length; i++) {
+            data[i] = decodeByte(data[i]);
+            for (int j = 0; j < 3; j++) {
+                int bitIndex = i * 3 + j;
+                int byteIndex = bitIndex / 8;
+                int bytePos = bitIndex % 8;
+                int bit = getBit(data[i], j * 2);
+                if (byteIndex >= outputBytes) {
+                    continue;
+                }
+                outData[byteIndex] = setBit(outData[byteIndex], bit, bytePos);
+            }
+        }
+        File outFile = new File("decoded.txt");
+        try (FileOutputStream fos = new FileOutputStream(outFile)) {
+            fos.write(outData);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
